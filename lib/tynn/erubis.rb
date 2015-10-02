0e88adfa664313aff682f5e190f475165ec9c7fb
Francesco Rodríguez <frodsan@me.com>
2015-10-02 02:04:20 +0200

    Add Tynn::Erubis helper.

---
+++ lib/tynn/erubis.rb
@@ -0,0 +1,17 @@
+require "erubis"
+require_relative "render"
+
+class Tynn
+  module Erubis
+    def self.setup(app, options = {})
+      options = options.dup
+
+      options[:options] ||= {}
+      options[:options] = {
+        escape_html: true
+      }.merge!(options[:options])
+
+      app.helpers(Tynn::Render, options)
+    end
+  end
+end
