0925d43fbc058d510b5039d0189767ba3f90dc62
Francesco Rodríguez <frodsan@me.com>
2015-10-03 17:15:50 +0200

    Add API documentation to Tynn::Environment.

--- lib/tynn/environment.rb
+++ lib/tynn/environment.rb
@@ -1,9 +1,26 @@
+# Adds helper methods to get and check the current environment.
+#
+#     require "tynn"
+#     require "tynn/environment"
+#
+#     Tynn.helpers(Tynn::Environment)
+#
+#     Tynn.environment  # => :development
+#
+#     Tynn.development? # => true
+#     Tynn.production?  # => false
+#     Tynn.test?        # => false
+#
+# By default, the environment is based on `ENV["RACK_ENV"]`.
+#
+#     Tynn.helpers(Tynn::Environment, env: ENV["RACK_ENV"])
+#
 module Tynn::Environment
   def self.setup(app, env: ENV["RACK_ENV"]) # :nodoc:
     app.settings[:environment] = (env || :development).to_sym
   end
 
-  module ClassMethods
+  module ClassMethods # :nodoc:
     def environment
       return settings[:environment]
     end
