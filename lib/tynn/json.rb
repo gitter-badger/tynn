cccb1bd46128bf915ca6fbd2f8199c15ddfa07ca
Francesco Rodríguez <frodsan@me.com>
2015-10-03 03:01:42 +0200

    Override Content-Type.

--- lib/tynn/json.rb
+++ lib/tynn/json.rb
@@ -4,7 +4,7 @@ module Tynn::JSON
   JSON_CONTENT_TYPE = "application/json".freeze # :nodoc:
 
   def json(data)
-    res.headers[Rack::CONTENT_TYPE] ||= JSON_CONTENT_TYPE
+    res.headers[Rack::CONTENT_TYPE] = JSON_CONTENT_TYPE
 
     res.write(data.to_json)
   end
