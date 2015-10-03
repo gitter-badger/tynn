7071e862d03a3b83069677f8743e2221f4b9f5d7
Francesco Rodríguez <frodsan@me.com>
2015-10-03 17:23:18 +0200

    Add API documentation to Tynn::DefaultMatcher helper.

--- lib/tynn/default_matcher.rb
+++ lib/tynn/default_matcher.rb
@@ -1,5 +1,22 @@
+# Adds a catch-all matcher.
+#
+#     require "tynn"
+#     require "tynn/default_matcher"
+#
+#     Tynn.helpers(Tynn::DefaultMatcher)
+#
+#     Tynn.define do
+#       authenticated? do
+#         # ...
+#       end
+#
+#       default do # on true
+#         # ...
+#       end
+#     end
+#
 module Tynn::DefaultMatcher
-  def default
+  def default # :nodoc:
     yield
 
     halt(res.finish)
