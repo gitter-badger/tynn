b02f31ced192755f26d6ece7e73bf6aa06f8e26b
Francesco Rodríguez <frodsan@me.com>
2015-10-06 17:35:26 +0200

    Remove settings from public API.
    Keep this for advanced users and extension writers.

--- lib/tynn/settings.rb
+++ lib/tynn/settings.rb
@@ -1,6 +1,6 @@
 class Tynn
-  module Settings
-    def self.deepclone(hash) # :nodoc:
+  module Settings # :nodoc: all
+    def self.deepclone(hash)
       default_proc, hash.default_proc = hash.default_proc, nil
 
       return Marshal.load(Marshal.dump(hash))
@@ -12,8 +12,8 @@ class Tynn
       return self.class.settings
     end
 
-    module ClassMethods
-      def inherited(subclass) # :nodoc:
+    module ClassMethods # :nodoc: all
+      def inherited(subclass)
         subclass.settings.replace(Tynn::Settings.deepclone(settings))
         subclass.settings.default_proc = proc { |h, k| h[k] = settings[k] }
       end
