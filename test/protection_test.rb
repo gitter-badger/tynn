f76156c8d857d51b7393f3f41bb02a62d75716dd
Francesco Rodríguez <frodsan@me.com>
2015-10-06 23:44:06 +0200

    Test that SSL plays well with session.

--- test/protection_test.rb
+++ test/protection_test.rb
@@ -1,4 +1,5 @@
 require_relative "../lib/tynn/protection"
+require_relative "../lib/tynn/session"
 
 test "includes secure headers" do
   Tynn.helpers(Tynn::Protection)
@@ -26,4 +27,40 @@ test "supports hsts options" do
   hsts = app.res.headers["Strict-Transport-Security"]
 
   assert_equal "max-age=100; preload", hsts
+end
+
+test "adds secure flag to session cookie" do
+  Tynn.helpers(Tynn::Protection, ssl: true)
+  Tynn.helpers(Tynn::Session, secret: "_this_must_be_random_")
+
+  Tynn.define do
+    root do
+      session[:foo] = "foo"
+    end
+  end
+
+  app = Tynn::Test.new
+  app.get("/", {}, "HTTPS" => "on")
+
+  session, _ = app.res.headers["Set-Cookie"].split("\n")
+
+  assert(/;\s*secure\s*(;|$)/i === session)
+end
+
+test "if session uses secure flag" do
+  Tynn.helpers(Tynn::Protection, ssl: true)
+  Tynn.helpers(Tynn::Session, secret: "_this_must_be_random_", secure: true)
+
+  Tynn.define do
+    root do
+      session[:foo] = "foo"
+    end
+  end
+
+  app = Tynn::Test.new
+  app.get("/", {}, "HTTPS" => "on")
+
+  session, _ = app.res.headers["Set-Cookie"].split("\n")
+
+  assert(/;\s*secure\s*(;|$)/i === session)
 end
