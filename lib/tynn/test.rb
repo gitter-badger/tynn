a710d40e016362ffb274deb885eac16a9585b58b
Francesco Rodríguez <frodsan@me.com>
2015-10-07 19:21:11 +0200

    Add docs to Tynn::Test.

--- lib/tynn/test.rb
+++ lib/tynn/test.rb
@@ -1,5 +1,27 @@
 require "rack/test"
 
+# Tynn::Test is a simple helper class to test your application.
+#
+#     require "tynn"
+#     require "tynn/test"
+#
+#     Tynn.define do
+#       root do
+#         res.write("hei")
+#       end
+#     end
+#
+#     app = Tynn::Test.new
+#     app.get("/")
+#
+#     200   == app.res.status # => true
+#     "hei" == app.res.body   # => true
+#
+# **NOTE:** Tynn doesn't ship with [rack-test][rack-test]. In order to
+# use this plugin, you need to install it.
+#
+# [rack-test]: http://rubygems.org/gems/rack-test
+#
 class Tynn::Test
   include Rack::Test::Methods
 
@@ -13,4 +35,16 @@ class Tynn::Test
 
   alias_method :res, :last_response
   alias_method :req, :last_request
+
+  ##
+  # :method: res
+  # :call-seq: res
+  #
+  # Alias for `last_response`.
+
+  ##
+  # :method: req
+  # :call-seq: req
+  #
+  # Alias for `last_request`.
 end
