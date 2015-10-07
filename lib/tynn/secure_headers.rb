97d4a3d7398a30500e234da9628d98ed826cc6eb
Francesco Rodríguez <frodsan@me.com>
2015-10-07 22:44:09 +0200

    Update Tynn::SecureHeaders docs.

--- lib/tynn/secure_headers.rb
+++ lib/tynn/secure_headers.rb
@@ -1,9 +1,34 @@
 # Adds security related HTTP headers.
 #
-#   require "tynn"
-#   require "tynn/secure_headers"
+#     require "tynn"
+#     require "tynn/secure_headers"
 #
-#   Tynn.helpers(Tynn::SecureHeaders)
+#     Tynn.helpers(Tynn::SecureHeaders)
+#
+# This helper applies the following headers:
+#
+# * **X-Content-Type-Options:** Prevents IE and Chrome from
+#   [content type sniffing][mime-sniffing].
+#
+# * **X-Frame-Options (XFO):** Provides [Clickjacking][clickjacking]
+#   protection. Check the [X-Frame-Options draft][x-frame-options] for
+#   more information.
+#
+# * **X-Permitted-Cross-Domain-Policies:** Restricts Adobe Flash Player's
+#   access to data. Check this [article][pcdp] for more information.
+#
+# * **X-XSS-Protection:** Enables the [XSS][xss] protection filter built
+#   into IE, Chrome and Safari. This filter is usually enabled by default,
+#   the use of this header is to re-enable it if it was disabled by the user.
+#
+# You can use <https://securityheaders.io> to asses the security related
+# HTTP headers used by your site.
+#
+# [clickjacking]: https://www.owasp.org/index.php/Clickjacking
+# [mime-sniffing]: https://msdn.microsoft.com/library/gg622941(v=vs.85).aspx
+# [pcdp]: https://www.adobe.com/devnet/adobe-media-server/articles/cross-domain-xml-for-streaming.html
+# [x-frame-options]: https://tools.ietf.org/html/draft-ietf-websec-x-frame-options-02
+# [xss]: https://www.owasp.org/index.php/Cross-site_Scripting_(XSS)
 #
 module Tynn::SecureHeaders
   HEADERS = {
@@ -13,7 +38,7 @@ module Tynn::SecureHeaders
     "X-XSS-Protection" => "1; mode=block"
   } # :nodoc:
 
-  def default_headers
+  def default_headers # :nodoc:
     return super.merge(HEADERS)
   end
 end
