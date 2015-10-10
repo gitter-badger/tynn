28b00b29a28dc84b13ce24569c5eba39b9d481a5
Francesco Rodríguez <frodsan@me.com>
2015-10-10 21:47:28 +0200

    Add documentation to Tynn::Session.

--- lib/tynn/session.rb
+++ lib/tynn/session.rb
@@ -1,3 +1,37 @@
+# Adds a simple cookie based session management.
+#
+#     require "tynn"
+#     require "tynn/session"
+#
+#     Tynn.helpers(Tynn::Session, secret: ENV["SESSION_SECRET"])
+#
+#     Tynn.define do
+#       get do
+#         user = User[session[:user_id]]
+#         # ...
+#       end
+#
+#       post do
+#         # ...
+#         session[:user_id] = user.id
+#         # ...
+#       end
+#     end
+#
+# Under the hood, it uses the [Rack::Session::Cookie][rack-session] middleware.
+# Thus, supports all the options available for this middleware.
+#
+#     Tynn.helpers(
+#       Tynn::Session,
+#       key: ENV["SESSION_KEY"],
+#       secret: ENV["SESSION_SECRET"],
+#       expire_after: nil,
+#       httponly: true,
+#       secure: true
+#     )
+#
+# [rack-session]: http://www.rubydoc.info/gems/rack/Rack/Session/Cookie
+#
 module Tynn::Session
   RACK_SESSION = "rack.session".freeze # :nodoc:
 
