4e459c687b39bdef9b5865bfeb6091a7c640965a
Francesco Rodríguez <frodsan@me.com>
2015-10-11 16:41:25 +0200

    Add documentation to Tynn::Session options.

--- lib/tynn/session.rb
+++ lib/tynn/session.rb
@@ -2,35 +2,62 @@
 # given, it signs the cookie data to ensure that it cannot be altered
 # by unauthorized means.
 #
+# ```
+# require "tynn"
+# require "tynn/session"
 #
-#     require "tynn"
-#     require "tynn/session"
+# Tynn.helpers(Tynn::Session, secret: "__change_me__")
 #
-#     Tynn.helpers(Tynn::Session, secret: "__change_me__")
+# Tynn.define do
+#   root do
+#     res.write(sprintf("hei %s", session[:username]))
+#   end
+#
+#   on(:username) do |username|
+#     session[:username] = username
+#   end
+# end
+# ```
 #
 # The following command generates a cryptographically secure secret ready
 # to use:
 #
-#     $ ruby -r securerandom -e "puts SecureRandom.hex(64)"
+# ```
+# $ ruby -r securerandom -e "puts SecureRandom.hex(64)"
+# ```
 #
 # It's important to keep the token secret. Knowing the token allows an
 # attacker to tamper the data. So, it's recommended to load the token
 # from the environment.
 #
-#     Tynn.helpers(Tynn::Session, secret: ENV["SESSION_SECRET"])
+# ```
+# Tynn.helpers(Tynn::Session, secret: ENV["SESSION_SECRET"])
+# ```
 #
 # Under the hood, Tynn::Session uses the [Rack::Session::Cookie][rack-session]
 # middleware. Thus, supports all the options available for this middleware.
 #
-#     Tynn.helpers(
-#       Tynn::Session,
-#       key: "app",
-#       secret: ENV["SESSION_SECRET"],
-#       expire_after: nil,
-#       httponly: true,
-#       secure: true
-#     )
+# * `:key` - the name of the cookie. Defaults to `"rack.session"`.
+# * `:httponly` - if `true`, sets the [HttpOnly][cookie-httponly] attribute.
+#   This mitigates the risk of client side scripting accessing the cookie.
+#   Defaults to `true`.
+# * `:secure` - if `true`, sets the [Secure][cookie-secure] attribute.
+#   This tells the browser to only transmit the cookie over HTTPS. Defaults
+#   to `false`.
 #
+# ```
+# Tynn.helpers(
+#   Tynn::Session,
+#   key: "app",
+#   secret: ENV["SESSION_SECRET"],
+#   expire_after: 36_000, # seconds
+#   httponly: true,
+#   secure: true
+# )
+# ```
+#
+# [cookie-httponly]: https://www.owasp.org/index.php/Session_Management_Cheat_Sheet#HttpOnly_Attribute
+# [cookie-secure]: https://www.owasp.org/index.php/Session_Management_Cheat_Sheet#Secure_Attribute
 # [rack-session]: http://www.rubydoc.info/gems/rack/Rack/Session/Cookie
 #
 module Tynn::Session
