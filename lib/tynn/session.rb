dc9b1f936aca8b7f6ccdcc95d2a0eee4db905af3
Francesco Rodríguez <frodsan@me.com>
2015-10-11 03:25:27 +0200

    Update Tynn::Session documentation.

--- lib/tynn/session.rb
+++ lib/tynn/session.rb
@@ -1,29 +1,30 @@
-# Adds a simple cookie based session management.
+# Adds a simple cookie based session management. If a secret token is given,
+# it signs the cookie data to ensure that cannot be altered by unauthorized
+# means.
+#
 #
 #     require "tynn"
 #     require "tynn/session"
 #
+#     Tynn.helpers(Tynn::Session, secret: "__insecure_secret__")
+#
+# The following command generates a cryptographically secure secret ready
+# to use:
+#
+#     $ ruby -r securerandom -e "puts SecureRandom.hex(64)"
+#
+# It's important to keep the token secret. Knowing the token allows an
+# attacker to tamper the data. So, it's recommended to load the token
+# from the environment.
+#
 #     Tynn.helpers(Tynn::Session, secret: ENV["SESSION_SECRET"])
 #
-#     Tynn.define do
-#       get do
-#         user = User[session[:user_id]]
-#         # ...
-#       end
-#
-#       post do
-#         # ...
-#         session[:user_id] = user.id
-#         # ...
-#       end
-#     end
-#
-# Under the hood, it uses the [Rack::Session::Cookie][rack-session] middleware.
-# Thus, supports all the options available for this middleware.
+# Under the hood, Tynn::Session uses the [Rack::Session::Cookie][rack-session]
+# middleware. Thus, supports all the options available for this middleware.
 #
 #     Tynn.helpers(
 #       Tynn::Session,
-#       key: ENV["SESSION_KEY"],
+#       key: "app",
 #       secret: ENV["SESSION_SECRET"],
 #       expire_after: nil,
 #       httponly: true,
