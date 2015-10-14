36ea97b578bac98abc4fa13a93501356e97dfe3f
Francesco Rodríguez <frodsan@me.com>
2015-10-14 21:11:10 +0200

    Add documentation to Tynn::Protection.

--- lib/tynn/protection.rb
+++ lib/tynn/protection.rb
@@ -1,6 +1,15 @@
 require_relative "secure_headers"
 
 class Tynn
+  # Adds security measures against common attacks.
+  #
+  # ```
+  # require "tynn"
+  # require "tynn/protection"
+  #
+  # Tynn.helpers(Tynn::Protection)
+  # ```
+  #
   module Protection
     def self.setup(app, ssl: false, hsts: {}) # :nodoc:
       app.helpers(Tynn::SecureHeaders)
