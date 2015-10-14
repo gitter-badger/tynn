c65c388f0ebeb4873419f5679f1da52a7710b65a
Francesco Rodríguez <frodsan@me.com>
2015-10-14 20:51:15 +0200

    Add documentation to Tynn::JSON.

--- lib/tynn/json.rb
+++ lib/tynn/json.rb
@@ -1,9 +1,38 @@
 require "json"
 
 class Tynn
+  # Adds helper methods for json generation.
+  #
+  # ```
+  # require "tynn"
+  # require "tynn/json"
+  #
+  # Tynn.helpers(Tynn::JSON)
+  # ```
+  #
   module JSON
     JSON_CONTENT_TYPE = "application/json".freeze # :nodoc:
 
+    # Calls `to_json` on `data` and writes the generated \JSON object into
+    # the response body. Also, It automatically sets the `Content-Type`
+    # header to `application/json`.
+    #
+    # ```
+    # Tynn.define do
+    #   on("hash") do
+    #     json(foo: "bar")
+    #   end
+    #
+    #   on("array") do
+    #     json([1, 2, 3])
+    #   end
+    #
+    #   on("to_json") do
+    #     json(Model.first)
+    #   end
+    # end
+    # ```
+    #
     def json(data)
       res.headers[Rack::CONTENT_TYPE] = JSON_CONTENT_TYPE
 
