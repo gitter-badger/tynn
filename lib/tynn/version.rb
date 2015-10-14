a4439a111ae1519dfa25bf3767a041f31833d6ca
Francesco Rodríguez <frodsan@me.com>
2015-10-14 05:00:59 +0200

    Release.

--- lib/tynn/version.rb
+++ lib/tynn/version.rb
@@ -1,7 +1,8 @@
 class Tynn # :nodoc:
   VERSION = [
-    MAJOR_VERSION = 0,
+    MAJOR_VERSION = 1,
     MINOR_VERSION = 0,
-    PATCH_VERSION = 4
-  ].join(".")
+    PATCH_VERSION = 0,
+    PRE_VERSION   = "rc1"
+  ].compact.join(".")
 end
