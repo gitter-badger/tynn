011cfc58d8103f8d030aa6638eaeb579eb9600a7
Francesco Rodríguez <frodsan@me.com>
2015-10-27 00:37:40 +0100

    [ci skip] Add docs to Tynn::Settings.

--- lib/tynn/settings.rb
+++ lib/tynn/settings.rb
@@ -1,5 +1,30 @@
 class Tynn
-  module Settings # :nodoc:
+  # Public: It provides a settings API for applications. This helper is
+  # included by default.
+  #
+  # Examples
+  #
+  #   require "tynn"
+  #
+  #   module AppName
+  #     def self.setup(app, app_name)
+  #       app.settings[:app_name] = app_name
+  #     end
+  #
+  #     module ClassMethods
+  #       def app_name
+  #         return settings[:app_name]
+  #       end
+  #     end
+  #   end
+  #
+  #   Tynn.helpers(AppName, "MyApp")
+  #
+  #   Tynn.app_name
+  #   # => "MyApp"
+  #
+  module Settings
+    # Internal: Returns a deep copy of a Hash.
     def self.deepclone(hash)
       default_proc, hash.default_proc = hash.default_proc, nil
 
@@ -8,12 +33,23 @@ class Tynn
       hash.default_proc = default_proc
     end
 
-    module ClassMethods # :nodoc:
+    module ClassMethods
+      # Internal: Copies settings into the subclass.
+      # If a setting is not found, checks parent's settings.
       def inherited(subclass)
         subclass.settings.replace(Tynn::Settings.deepclone(settings))
         subclass.settings.default_proc = proc { |h, k| h[k] = settings[k] }
       end
 
+      # Returns a Hash with the application settings.
+      #
+      # Examples
+      #
+      #   Tynn.set(:environment, :development)
+      #
+      #   Tynn.settings
+      #   # => { :environment => :development }
+      #
       def settings
         return @settings ||= {}
       end
