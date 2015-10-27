1bb9d1aedefbde9dd82b03d97e1027f431ddd0e9
Francesco Rodríguez <frodsan@me.com>
2015-10-27 01:47:22 +0100

    [ci skip] Update docs to tomdoc format.

--- docs/rdoc/tynn.rb
+++ docs/rdoc/tynn.rb
@@ -1,39 +1,50 @@
 class Tynn
   # The following methods are inherited by Syro:
 
-  ##
-  # :method: halt(response)
+  # Public: Immediately stops the request and returns +response+
+  # as per Rack's specification.
   #
-  # Immediately stops the request and returns `response` as per
-  # Rack's specification.
+  # response - An Array of three elements: status, headers and body.
   #
-  # ```
-  # halt([200, { "Content-Type" => "text/html" }, ["hello"]])
-  # halt([res.status, res.headers, res.body])
-  # halt(res.finish)
-  # ```
+  # Examples
+  #
+  #   halt([200, { "Content-Type" => "text/html" }, ["hello"]])
+  #   halt([res.status, res.headers, res.body])
+  #   halt(res.finish)
+  #
+  # Signature
+  #
+  #   halt(response)
+  #
+  # Inherited by Syro::Response.
 
-  ##
-  # :method: req
+  # Public: Returns the incoming request object. This object is an
+  # instance of Tynn::Request.
   #
-  # Returns the incoming request object. This object is an instance
-  # of Tynn::Request.
+  # Examples
   #
-  # ```
-  # req.post?      # => true
-  # req.params     # => { "username" => "bob", "password" => "secret" }
-  # req[:username] # => "bob"
-  # ```
+  #   req.post?      # => true
+  #   req.params     # => { "username" => "bob", "password" => "secret" }
+  #   req[:username] # => "bob"
+  #
+  # Signature
+  #
+  #   req()
+  #
+  # Inherited by Syro::Response.
 
-  ##
-  # :method: res
-  #
-  # Returns the current response object. This object is an instance
+  # Public: Returns the current response object. This object is an instance
   # of Tynn::Response.
   #
-  # ```
-  # res.status = 200
-  # res["Content-Type"] = "text/html"
-  # res.write("<h1>Welcome back!</h1>")
-  # ```
+  # Examples
+  #
+  #   res.status = 200
+  #   res["Content-Type"] = "text/html"
+  #   res.write("<h1>Welcome back!</h1>")
+  #
+  # Signature
+  #
+  #   res()
+  #
+  # Inherited by Syro::Response.
 end
