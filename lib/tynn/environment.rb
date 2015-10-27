f36c0f2b7ccef68b2f093b6f747adf8d4177e3ef
Francesco Rodríguez <frodsan@me.com>
2015-10-27 02:29:23 +0100

    [ci skip] Update Tynn::Environment docs.

--- lib/tynn/environment.rb
+++ lib/tynn/environment.rb
@@ -21,23 +21,69 @@ class Tynn
   #   Tynn.helpers(Tynn::Environment, env: ENV["RACK_ENV"])
   #
   module Environment
-    def self.setup(app, env: ENV["RACK_ENV"]) # :nodoc:
+    # Internal: Configures current environment.
+    def self.setup(app, env: ENV["RACK_ENV"])
       app.set(:environment, (env || :development).to_sym)
     end
 
-    module ClassMethods # :nodoc:
+    module ClassMethods
+      # Public: Returns current environment.
+      #
+      # Examples
+      #
+      #   Tynn.environment
+      #   # => :development
+      #
+      #   Tynn.set(:environment, :test)
+      #
+      #   Tynn.environment
+      #   # => :test
+      #
       def environment
         return settings[:environment]
       end
 
+      # Public: Returns +true+ if the current environment is +:development+.
+      # Otherwise returns +false+.
+      #
+      # Examples
+      #
+      #   Tynn.set(:environment, :test)
+      #   Tynn.development? # => false
+      #
+      #   Tynn.set(:environment, :development)
+      #   Tynn.development? # => true
+      #
       def development?
         return environment == :development
       end
 
+      # Public: Returns +true+ if the current environment is +:test+.
+      # Otherwise returns +false+.
+      #
+      # Examples
+      #
+      #   Tynn.set(:environment, :development)
+      #   Tynn.test? # => false
+      #
+      #   Tynn.set(:environment, :test)
+      #   Tynn.test? # => true
+      #
       def test?
         return environment == :test
       end
 
+      # Public: Returns +true+ if the current environment is +:production+.
+      # Otherwise returns +false+.
+      #
+      # Examples
+      #
+      #   Tynn.set(:environment, :development)
+      #   Tynn.production? # => false
+      #
+      #   Tynn.set(:environment, :production)
+      #   Tynn.production? # => true
+      #
       def production?
         return environment == :production
       end
