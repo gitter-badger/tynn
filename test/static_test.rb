b1750d10ab438ef76aa65f88578adc3004f0f1f6
Francesco Rodríguez <frodsan@protonmail.ch>
2016-05-03 22:19:34 -0500

    Refactor Tynn::Static tests.

--- test/static_test.rb
+++ test/static_test.rb
@@ -4,14 +4,12 @@ require_relative "helper"
 require_relative "../lib/tynn/static"
 
 class StaticTest < Tynn::TestCase
-  class App < Tynn
-    plugin Tynn::Static, %w(/test), root: Dir.pwd
-
-    define {}
-  end
-
   test "serves static files" do
-    app = Tynn::Test.new(App)
+    Tynn.plugin(Tynn::Static, %w(/test), root: Dir.pwd)
+
+    Tynn.define {}
+
+    app = Tynn::Test.new
     app.get("/test/static_test.rb")
 
     assert_equal File.read(__FILE__), app.res.body
